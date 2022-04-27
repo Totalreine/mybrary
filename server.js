263ceae7d90cfaa399ab62f7388a6d004e5e4515
Juan Carlos <4522@holbertonschool.com>
2022-04-27 12:51:34 -0500

    second one

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -18,7 +20,8 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use('/', indexRouter)
-
+app.use('/authors', authorRouter)
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 
 mongoose.connect(process.env.DATABASE_URL, 
